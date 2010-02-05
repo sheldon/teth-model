314d8776e85a8390153d735c3dbeee7e8dd5be58
Sheldon <sheldon.els@gmail.com>
2010-02-05 00:36:59 +0000

    allow construction of models passing in their data. collections return vanilla models by default.

--- TethCollection.php
+++ TethCollection.php
@@ -2,10 +2,11 @@
 class TethCollection implements Iterator, ArrayAccess, Countable {
   public $collection;
   public $position = 0;
+  public $model = "TethModel";
 
   //Iterator methods
   public function rewind(){ $this->position = 0; }
-  public function current(){ return $this->collection[$this->position]; }
+  public function current(){ return new $this->model($this->collection[$this->position]); }
   public function key(){ return $this->position; }
   public function next(){ ++$this->position; }
   public function valid(){ return isset($this->collection[$this->position]); }
@@ -14,7 +15,7 @@ class TethCollection implements Iterator, ArrayAccess, Countable {
   public function offsetSet($offset, $value){ $this->collection[$offset] = $value; }
   public function offsetExists($offset){ return isset($this->collection[$offset]); }
   public function offsetUnset($offset){ unset($this->collection[$offset]); }
-  public function offsetGet($offset){ return $this->offsetExists($offset) ? $this->collection[$offset] : null; }
+  public function offsetGet($offset){ return $this->offsetExists($offset) ? new $this->model($this->collection[$offset]) : null; }
 
   //Countable method
   public function count(){ return count($collection); }
--- TethModel.php
+++ TethModel.php
@@ -1,6 +1,7 @@
 <?php
 class TethModel {
-  public $data = array();
+  public $data;
+  function __construct($data = array()){ $this->data = $data; }
   public function __set($name,$value){ return $this->data[$name] = $value; }
   public function __get($name){ return $this->data[$name]; }
 }?>
