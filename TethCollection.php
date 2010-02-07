936db91c998f180ae3c6e2d39718ca12e19bea42
Sheldon <sheldon.els@gmail.com>
2010-02-07 13:02:22 +0000

    collections can now hold different models together in the same collection. we figure out model on fetch.

--- TethCollection.php
+++ TethCollection.php
@@ -1,26 +1,20 @@
 <?php
 class TethCollection implements Iterator, ArrayAccess, Countable {
-  public $model;
   public $collection;
   public $position = 0;
 
-  function __construct($collection = null, $model = "TethModel"){
-    $this->model = $model;
-    $this->collection = $collection;
-  }
+  function __construct($collection = null){ $this->collection = $collection; }
 
   public static function get(){
     $class = get_called_class();
     $obj = new $class;
-    $num_args = func_num_args();
-    if($num_args > 0) $obj->collection = func_get_arg(0);
-    if($num_args > 1) $obj->model = func_get_arg(1);
+    if(func_num_args() > 0) $obj->collection = func_get_arg(0);
     return $obj;
   }
 
   //Iterator methods
   public function rewind(){ $this->position = 0; }
-  public function current(){ return new $this->model($this->collection[$this->position]); }
+  public function current(){ $this->offsetGet($this->position); }
   public function key(){ return $this->position; }
   public function next(){ ++$this->position; }
   public function valid(){ return isset($this->collection[$this->position]); }
@@ -29,7 +23,14 @@ class TethCollection implements Iterator, ArrayAccess, Countable {
   public function offsetSet($offset, $value){ $this->collection[$offset] = $value; }
   public function offsetExists($offset){ return isset($this->collection[$offset]); }
   public function offsetUnset($offset){ unset($this->collection[$offset]); }
-  public function offsetGet($offset){ return $this->offsetExists($offset) ? new $this->model($this->collection[$offset]) : null; }
+  public function offsetGet($offset){
+    if(!$this->offsetExists($offset)) return null;
+    if(is_array($this->collection[$offset]) && ($class = $this->collection[$offset]["teth_class"]))
+      unset($this->collection[$offset]["teth_class"]);
+    else
+      $class = Config::$settings['classes']['default_model']['class'];
+    return new $class($this->collection[$offset]);
+  }
 
   //Countable method
   public function count(){ return count($this->collection); }
